aa88788f97b05c79ec3cb07f335b470194257b82
Arafat Rahman <arafatrahman0012@gmail.com>
2026-03-13 15:34:32 +0600

    chore: selection sort implemented

---
+++ sorting/selection.py
@@ -0,0 +1,17 @@
+arr = [64, 25, 12, 22, 11]
+
+def selectionSort(collection):
+    n = len(collection)
+    for i in range(n - 1):
+        minIndex = i
+        for j in range(i + 1, n):
+            if collection[minIndex] > collection[j]:
+                minIndex = j
+        collection[i], collection[minIndex] = collection[minIndex], collection[i]
+
+    return collection
+
+newArr = selectionSort(arr[:])
+
+print(f"Unsorted arr: {arr}")
+print(f"Sorted arr: {newArr}")
